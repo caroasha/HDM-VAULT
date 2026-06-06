ca77b88e789a2d675492d0ea8c4b3bb1cff55b51
Davix HDM <davismcintyre5@gmail.com>
2026-06-06 11:29:16 +0300

    Updated Path

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './store/AuthContext';
 import { ThemeProvider } from './store/ThemeContext';
@@ -9,12 +9,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
+    <HashRouter>
       <ThemeProvider>
         <AuthProvider>
           <ToastProvider>
@@ -22,6 +17,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           </ToastProvider>
         </AuthProvider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
